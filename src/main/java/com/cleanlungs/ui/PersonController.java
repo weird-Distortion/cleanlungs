bb3273c83a7d9ad1b9586c88bb47e5efc573679e
weird-Distortion <ilyalilison@gmail.com>
2018-07-04 12:03:47 +0200

    Index page edited

--- src/main/java/com/cleanlungs/ui/PersonController.java
+++ src/main/java/com/cleanlungs/ui/PersonController.java
@@ -3,10 +3,12 @@ package com.cleanlungs.ui;
 import com.cleanlungs.Person;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -20,8 +22,22 @@ public class PersonController {
     @PersistenceContext
     EntityManager entityManager;
 
+    private Date registeredAfter;
+
+    @Produces
+    @Named
     public List<Person> getPersons() {
-        TypedQuery<Person> query = entityManager.createQuery("select e from Person e", Person.class);
+        TypedQuery<Person> query = entityManager
+                .createQuery("select e from Person e where e.registrationDate >= :registeredAfter", Person.class);
+        query.setParameter("registeredAfter", registeredAfter);
         return query.getResultList();
+    }
+
+    public Date getRegisteredAfter() {
+        return registeredAfter;
+    }
+
+    public void setRegisteredAfter(final Date registeredAfter) {
+        this.registeredAfter = registeredAfter;
     }
 }
