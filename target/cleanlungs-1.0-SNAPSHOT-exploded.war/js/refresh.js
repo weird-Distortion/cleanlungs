27cd3f3c4896a30689d351c0655cee870d9eebc2
weird-Distortion <ilyalilison@gmail.com>
2018-08-16 15:56:51 +0200

    refresh.js - done
    Refreshing javascript is done. Status is checked every second. Status color depends on status value: GREEN - OK, GRAY - NOT WORKING, RED - DETECTED. AJAX and JQuery used.

--- target/cleanlungs-1.0-SNAPSHOT-exploded.war/js/refresh.js
+++ target/cleanlungs-1.0-SNAPSHOT-exploded.war/js/refresh.js
@@ -1,54 +1,18 @@
 $(document).ready(function () {
-  // let timer = setTimeout(function () {
-  // $(document).ready(function () {
-  $('.status').each(function () {
-    if ($(this).text() === 'OK') {
-      $(this).css({'background-color': 'green'});
-      console.log("green worked"); //logging
-      // clearTimeout(timer);
-    }
-    if ($(this).text() === 'NOT WORKING') {
-      $(this).css({'background-color': 'gray'});
-      console.log("gray worked"); //logging
-      // clearTimeout(timer);
-    }
-    if ($(this).text() === 'DETECTED') {
-      $(this).css({'background-color': 'red'});
-      console.log("red worked"); //logging
-      // clearTimeout(timer);
-    }
-  });
-  // }, 5000);
-  // });
+  setInterval(function () {
+    $('.status').each(function (index) {
+      if ($(this).text() === 'OK') {
+        $(this).css({'background-color': 'green'});
+        console.log("green worked");
+      }
+      if ($(this).text() === 'NOT WORKING') {
+        $(this).css({'background-color': 'gray'});
+        console.log("gray worked");
+      }
+      if ($(this).text() === 'DETECTED') {
+        $(this).css({'background-color': 'red'});
+        console.log("red worked");
+      }
+    });
+  }, 1000);
 });
-
-
-// autoRefresh();
-//
-// function autoRefresh() {
-//   "use strict";
-//   let timer = setTimeout(function () {
-//     $(document).ready(function () {
-//       $('.status').each(function (index) {
-//         if ($(this).text() === 'OK') {
-//           $(this).css({'background-color': 'green'});
-//           console.log("green worked");
-//           clearTimeout(timer);
-//           autoRefresh();
-//         }
-//         if ($(this).text() === 'NOT WORKING') {
-//           $(this).css({'background-color': 'gray'});
-//           console.log("gray worked");
-//           clearTimeout(timer);
-//           autoRefresh();
-//         }
-//         if ($(this).text() === 'DETECTED') {
-//           $(this).css({'background-color': 'red'});
-//           console.log("red worked");
-//           clearTimeout(timer);
-//           autoRefresh();
-//         }
-//       });
-//     });
-//   }, 5000);
-// }
