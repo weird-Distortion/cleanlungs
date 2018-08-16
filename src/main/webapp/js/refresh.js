2007ac09febad95d1375499996bfc7322856f7e3
weird-Distortion <ilyalilison@gmail.com>
2018-08-16 14:30:27 +0200

    Console errors fixed.
    TypeError: n.easing[this.easing] is not a function - fixed. Still no
    idea why it apperas :D

--- src/main/webapp/js/refresh.js
+++ src/main/webapp/js/refresh.js
@@ -1,40 +1,54 @@
-autoRefresh();
+$(document).ready(function () {
+  // let timer = setTimeout(function () {
+  // $(document).ready(function () {
+  $('.status').each(function () {
+    if ($(this).text() === 'OK') {
+      $(this).css({'background-color': 'green'});
+      console.log("green worked"); //logging
+      // clearTimeout(timer);
+    }
+    if ($(this).text() === 'NOT WORKING') {
+      $(this).css({'background-color': 'gray'});
+      console.log("gray worked"); //logging
+      // clearTimeout(timer);
+    }
+    if ($(this).text() === 'DETECTED') {
+      $(this).css({'background-color': 'red'});
+      console.log("red worked"); //logging
+      // clearTimeout(timer);
+    }
+  });
+  // }, 5000);
+  // });
+});
 
-var timer;
 
-function autoRefresh() {
-  "use strict";
-  timer = setTimeout(function () {
-    $(document).ready(function () {
-      $('.status').each(function (index) {
-        if ($(this).text() === 'OK') {
-          $(this).css({'background-color': 'green'});
-          console.log("green worked");
-          clearTimeout(timer);
-          autoRefresh();
-        }
-        if ($(this).text() === 'NOT WORKING') {
-          $(this).css({'background-color': 'gray'});
-          console.log("gray worked");
-          clearTimeout(timer);
-          autoRefresh();
-        }
-        if ($(this).text() === 'DETECTED') {
-          $(this).css({'background-color': 'red'});
-          console.log("red worked");
-          clearTimeout(timer);
-          autoRefresh();
-        }
-      });
-    });
-  }, 5000);
-}
-
+// autoRefresh();
+//
 // function autoRefresh() {
 //   "use strict";
 //   let timer = setTimeout(function () {
-//     alert("works");
-//     autoRefresh();
-//     console.log("worked");
-//   }, 1000);
+//     $(document).ready(function () {
+//       $('.status').each(function (index) {
+//         if ($(this).text() === 'OK') {
+//           $(this).css({'background-color': 'green'});
+//           console.log("green worked");
+//           clearTimeout(timer);
+//           autoRefresh();
+//         }
+//         if ($(this).text() === 'NOT WORKING') {
+//           $(this).css({'background-color': 'gray'});
+//           console.log("gray worked");
+//           clearTimeout(timer);
+//           autoRefresh();
+//         }
+//         if ($(this).text() === 'DETECTED') {
+//           $(this).css({'background-color': 'red'});
+//           console.log("red worked");
+//           clearTimeout(timer);
+//           autoRefresh();
+//         }
+//       });
+//     });
+//   }, 5000);
 // }
